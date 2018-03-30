98c0e03367b58aeee339861692be270c6c3c0f87
fushanshan <fushanshan@qiyi.com>
2018-03-30 20:14:22 +0800

    note

---
+++ 02_logistic_regression/codes/logistic_regression.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/3/30 13:47
+# @Author  : Shanshan Fu
+# @File    : logistic_regression.py  :
+# @Contact : 33sharewithu@gmail.com
+
+class LogisticRegression:
+    def __init__(self, alpha = 0.01, max_iter = 100):
+        self.alpha = alpha
+        self.max_iter = max_iter
+
