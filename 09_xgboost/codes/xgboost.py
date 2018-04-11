f53c5d482fb76f6cef847ab7b6e6a020df1156c9
fushanshan <fushanshan@qiyi.com>
2018-04-11 16:06:20 +0800

    nb

--- 09_xgboost/codes/xgboost.py
+++ 09_xgboost/codes/xgboost.py
@@ -1,2 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import numpy as np
+
+class MultinomialNB(object):
+
+    def __init__(self, alpha):
+        self.alpha = alpha
+        self.classes = None
+
+    def fit(self, X, y):
+        self.classes = np.unique(y)
---
+++ 15_naive_bayes/codes/multinomial_nb.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/4/10 17:37
+# @Author  : Shanshan Fu
+# @File    : multinomial_nb.py  :
+# @Contact : 33sharewithu@gmail.com
